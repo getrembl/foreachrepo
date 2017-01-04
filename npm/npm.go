73a91b13115a6a5255bcee534cdc707afb3f1d39
Samuel Rossille <samuel.rossille@gmail.com>
2017-01-05 00:50:59 +0100

    WIP

--- npm/npm.go
+++ npm/npm.go
@@ -6,6 +6,8 @@ import (
 	"io/ioutil"
 	"path/filepath"
 	"os"
+	"encoding/json"
+	"os/exec"
 )
 
 const SPACES = "\\s*"
@@ -29,15 +31,6 @@ func (D *DependencyNotFound) Error() string {
 	return "Dependency " + D.dependency + " not found in package.json"
 }
 
-type DependencyUpToDate struct {
-	packageContent string
-	dependency     string
-}
-
-func (D *DependencyUpToDate) Error() string {
-	return "Dependency " + D.dependency + " is already up to date"
-}
-
 type InvalidPackageJsonContent struct {
 	packageContent string
 	dependency     string
@@ -47,7 +40,28 @@ func (D *InvalidPackageJsonContent) Error() string {
 	return "Invalid package.json content " + D.packageContent
 }
 
-func GetUpdatedPackageContent(packageContent string, dependency string, version string) (string, error) {
+type NpmListOutput struct {
+	Dependencies map[string]map[string]string
+}
+
+func ExtractVersions(data NpmListOutput) map[string]string {
+	result := make(map[string]string)
+	dependencies := data.Dependencies
+	for name, properties := range dependencies {
+		result[name] = properties["version"]
+	}
+	return result
+}
+
+func ParseNpmListOutput(output []byte) map[string]string {
+	var data NpmListOutput
+
+	json.Unmarshal(output, &data)
+
+	return ExtractVersions(data)
+}
+
+func UpdateDependency(packageContent string, dependency string, version string) (string, error) {
 	elements := []string{
 		SPACES,
 		quote(group(regexp.QuoteMeta(dependency))),
@@ -69,24 +83,31 @@ func GetUpdatedPackageContent(packageContent string, dependency string, version 
 		match := matches[0]
 		dependencyVersionStart := match[4]
 		dependencyVersionEnd := match[5]
-		currentVersion := packageContent[dependencyVersionStart:dependencyVersionEnd]
-		if currentVersion == version {
-			return "", &DependencyUpToDate{packageContent, dependency}
-		}
 		return packageContent[:dependencyVersionStart] + version + packageContent[dependencyVersionEnd:], nil
 	default:
 		return "", &InvalidPackageJsonContent{packageContent, dependency}
 	}
 }
 
+func UpdateDependencies(packageContent string, updates map[string]string) (string, error) {
+	var err error
+	for name, version := range updates {
+		packageContent, err = UpdateDependency(packageContent, name, version)
+		if err != nil {
+			return "", err
+		}
+	}
+	return packageContent, nil
+}
+
 // Exists reports whether the named file or directory exists.
 func exists(name string) bool {
-    if _, err := os.Stat(name); err != nil {
-    if os.IsNotExist(err) {
-                return false
-            }
-    }
-    return true
+	if _, err := os.Stat(name); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
 }
 
 type NoPackageJson struct {
@@ -107,7 +128,7 @@ func UpdatePackage(dir string, dependency string, version string) error {
 		return err
 	}
 	packageContent := string(bytes)
-	updatedPackageContent, err := GetUpdatedPackageContent(packageContent, dependency, version)
+	updatedPackageContent, err := UpdateDependency(packageContent, dependency, version)
 	if err != nil {
 		return err
 	}
@@ -115,5 +136,69 @@ func UpdatePackage(dir string, dependency string, version string) error {
 	if err != nil {
 		return err
 	}
+	return nil
+}
+
+func Exec(dir string, name string, elements ...string) error {
+	cmd := exec.Command(name, elements...)
+	cmd.Dir = dir
+	if err := cmd.Start(); err != nil {
+		return err
+	}
+	if err := cmd.Wait(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func ExecNpmList(dir string) (NpmListOutput, error) {
+	cmd := exec.Command("npm", "list", "--depth", "0", "--json")
+	cmd.Dir = dir
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return nil, err
+	}
+	if err := cmd.Start(); err != nil {
+		return nil, err
+	}
+	var result NpmListOutput
+	if err := json.NewDecoder(stdout).Decode(&result); err != nil {
+		return nil, err
+	}
+	if err := cmd.Wait(); err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func FreezePackage(dir string) error {
+	packageFile := filepath.Join(dir, "package.json")
+	if !exists(packageFile) {
+		return &NoPackageJson{dir}
+	}
+	bytes, err := ioutil.ReadFile(packageFile)
+	if err != nil {
+		return err
+	}
+	packageContent := string(bytes)
+
+	Exec(dir, "rm", "-rf", "node_modules")
+	Exec(dir, "npm", "i")
+	npmListOutput, err := ExecNpmList(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	versions := ExtractVersions(npmListOutput)
+
+	updatedPackageContent, err := UpdateDependencies(packageContent, versions)
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile(packageFile, []byte(updatedPackageContent), 0644)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
--- npm/npm_test.go
+++ npm/npm_test.go
@@ -6,6 +6,9 @@ import (
 	"io/ioutil"
 	"path/filepath"
 	"os"
+	"os/exec"
+	"log"
+	"encoding/json"
 )
 
 const SAMPLE_PACKAGE_CONTENT = `{
@@ -86,14 +89,14 @@ const EXPECTED_UPDATED_PACKAGE_CONTENT = `{
 }
 `
 
-func TestGetUpdatedPackageContentSuccess(t *testing.T) {
-	updated, err := GetUpdatedPackageContent(SAMPLE_PACKAGE_CONTENT, "bunyan", "1.8.2")
+func TestUpdateDependencySuccess(t *testing.T) {
+	updated, err := UpdateDependency(SAMPLE_PACKAGE_CONTENT, "bunyan", "1.8.2")
 	assert.Nil(t, err)
 	assert.Equal(t, EXPECTED_UPDATED_PACKAGE_CONTENT, updated)
 }
 
-func TestGetUpdatedPackageContentNotFound(t *testing.T) {
-	updated, err := GetUpdatedPackageContent(SAMPLE_PACKAGE_CONTENT, "not-a-dependency", "1.8.2")
+func TestUpdateDependencyNotFound(t *testing.T) {
+	updated, err := UpdateDependency(SAMPLE_PACKAGE_CONTENT, "not-a-dependency", "1.8.2")
 	assert.Equal(t, "", updated)
 	assert.NotNil(t, err)
 	errMessage := err.Error()
@@ -101,13 +104,56 @@ func TestGetUpdatedPackageContentNotFound(t *testing.T) {
 	assert.Contains(t, errMessage, "not found")
 }
 
-func TestGetUpdatedPackageContentUpToDate(t *testing.T) {
-	updated, err := GetUpdatedPackageContent(SAMPLE_PACKAGE_CONTENT, "bunyan", "~1.8.1")
-	assert.Equal(t, "", updated)
-	assert.NotNil(t, err)
-	errMessage := err.Error()
-	assert.Contains(t, errMessage, "bunyan")
-	assert.Contains(t, errMessage, "up to date")
+const EXPECTED_UPDATED_PACKAGE_CONTENT_MULTIPLE_DEPS = `{
+  "name": "express-middleware",
+  "version": "1.4.0",
+  "description": "Set of middlewares for Chauffeur-Privé",
+  "keywords": [
+    "express",
+    "middleware",
+    "chauffeur-privé"
+  ],
+  "main": "index.js",
+  "dependencies": {
+    "@chauffeur-prive/i18n": "1.2.0",
+    "accept-language-parser": "1.3.0",
+    "bunyan": "1.8.2"
+  },
+  "devDependencies": {
+    "chai": "3.5.0",
+    "eslint-config-cp": "transcovo/eslint-config-cp#1.1.0"
+  },
+  "scripts": {
+    "test": "npm run lint && npm run coverage"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/transcovo/express-middlewares.git"
+  },
+  "engines": {
+    "node": ">=4.2.2",
+    "npm": ">=2.14.7"
+  },
+  "bugs": {
+    "url": "https://github.com/transcovo/express-middlewares/issues"
+  },
+  "homepage": "https://github.com/transcovo/express-middlewares#readme",
+  "author": "Chauffeur-Privé",
+  "license": "Apache-2.0"
+}
+`
+
+func TestUpdateDependencies(t *testing.T) {
+	updates := map[string]string{
+		"@chauffeur-prive/i18n": "1.2.0",
+		"accept-language-parser": "1.3.0",
+		"bunyan": "1.8.2",
+		"chai": "3.5.0",
+		"eslint-config-cp": "transcovo/eslint-config-cp#1.1.0",
+	}
+	updated, err := UpdateDependencies(SAMPLE_PACKAGE_CONTENT, updates)
+	assert.Nil(t, err)
+	assert.Equal(t, EXPECTED_UPDATED_PACKAGE_CONTENT_MULTIPLE_DEPS, updated)
 }
 
 func TestUpdatePackage(t *testing.T) {
@@ -129,4 +175,54 @@ func TestUpdatePackage(t *testing.T) {
 	}
 	updatedPackageContent := string(bytes)
 	assert.Equal(t, EXPECTED_UPDATED_PACKAGE_CONTENT, updatedPackageContent)
+}
+
+const SAMPLE_NPM_LIST_OUTPUT = `{
+  "name": "c3po",
+  "version": "0.1.0",
+  "problems": [
+    "peer dep missing: eslint-plugin-react@^5.0.1, required by eslint-config-airbnb@9.0.1"
+  ],
+  "dependencies": {
+    "@chauffeur-prive/mongo-helper": {
+      "version": "2.1.0",
+      "from": "@chauffeur-prive/mongo-helper@>=2.1.0 <3.0.0",
+      "resolved": "https://registry.npmjs.org/@chauffeur-prive/mongo-helper/-/mongo-helper-2.1.0.tgz"
+    },
+    "express-middleware": {
+      "version": "1.5.0",
+      "from": "express-middleware@>=1.3.2 <2.0.0",
+      "resolved": "https://registry.npmjs.org/express-middleware/-/express-middleware-1.5.0.tgz"
+    }
+  }
+}`
+
+func TestParseNpmListOutput(t *testing.T) {
+	versions := ParseNpmListOutput([]byte(SAMPLE_NPM_LIST_OUTPUT))
+
+	assert.Len(t, versions, 2)
+	assert.Equal(t, "2.1.0", versions["@chauffeur-prive/mongo-helper"])
+	assert.Equal(t, "1.5.0", versions["express-middleware"])
+}
+
+func TestFreezePackage(t *testing.T) {
+	dir, mkdir_err := ioutil.TempDir("", "")
+	if mkdir_err != nil {
+		panic(mkdir_err)
+	}
+	defer os.RemoveAll(dir)
+	packageFile := filepath.Join(dir, "package.json")
+	fileWriteErr := ioutil.WriteFile(packageFile, []byte(SAMPLE_PACKAGE_CONTENT), 0644)
+	if fileWriteErr != nil {
+		panic(fileWriteErr)
+	}
+	updateErr := FreezePackage(dir)
+	assert.Nil(t, updateErr)
+	bytes, err := ioutil.ReadFile(packageFile)
+	if err != nil {
+		panic(err)
+	}
+	updatedPackageContent := string(bytes)
+	assert.NotContains(t, updatedPackageContent, "^")
+	assert.NotContains(t, updatedPackageContent, "~")
 }
